54672c9e743acb422ee32dfcb8f6ba2ca4eea24b
Edward <97419684+edward99enclave@users.noreply.github.com>
2022-02-17 09:53:03 +0700

    master: update - edit title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edward: Simple App Two
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Simple App Two
         </a>
       </header>
     </div>
